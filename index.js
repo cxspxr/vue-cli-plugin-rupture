16a33d57c06e8bb4ebd45ff25c4d86db462563c4
casper <ycasperinho@gmail.com>
2018-09-30 04:23:27 +0300

    return "use" back

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const rupture = require('rupture');
 
 module.exports = function(API) {
   return API.configureWebpack(function() {
-    return {
+    {
       module: {
         rules: [
           {
             test: /\.(styl|sss|stylus)$/,
             loader: 'stylus-loader',
-            import: [
-              '~rupture/rupture/index.styl'
-            ]
+            use: [rupture()]
           }
         ]
       }
